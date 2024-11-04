5ced067ea055b1507bb43a5760b8b1fa256cefb5
Keneth <knthnolasco@gmail.com>
2024-11-04 15:20:14 -0600

    Primer Commit
    Creación del proyecto

---
+++ industrialPasantias/src/main/java/com/industrial/pasantias/IndustrialPasantiasApplication.java
@@ -0,0 +1,13 @@
+package com.industrial.pasantias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialPasantiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustrialPasantiasApplication.class, args);
+	}
+
+}
---
+++ industrialPasantias/src/test/java/com/industrial/pasantias/IndustrialPasantiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.industrial.pasantias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustrialPasantiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
