c322ce32bc4e92a8d1a03144c4ce0049e18e0f84
DMancia03 <diego.mancia.03@gmail.com>
2024-12-01 21:35:05 -0600

    Agregando modelo y repositorio para estudiante V1

---
+++ industrialPasantias/src/main/java/com/industrial/pasantias/Model/Estudiante.java
@@ -0,0 +1,66 @@
+package com.industrial.pasantias.Model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ESTUDIANTE")
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODIGO_PASANTE")
+    private String codigoPasante;
+    
+    @Column(name = "CARNET", nullable = false)
+    private String carnet;
+
+    @Column(name = "NOMBRES", nullable = false)
+    private String nombres;
+
+    @Column(name = "APELLIDOS", nullable = false)
+    private String apellidos;
+
+    @Column(name = "TELEFONO", nullable = false)    
+    private String telefono;
+
+    @Column(name = "TELEFONO2")
+    private String telefono2;
+
+    @Column(name = "CORREO", nullable = false)
+    private String correo;
+
+    @Column(name = "HOLA_DE_VIDA", nullable = false)
+    private String hojaDeVida;
+
+    @Column(name = "ID_CARRERA", nullable = false)
+    private Integer idCarrera;
+
+    @Column(name = "FOTO_URL", nullable = false)
+    private String fotoUrl;
+
+    @Column(name = "FECHA_CREA", nullable = false)
+    private LocalDateTime fechaCre;
+
+    @Column(name = "FECHA_MOD")
+    private LocalDateTime fechaMod;
+
+    // Constructor con parámetros
+    public Estudiante(String carnet, String nombres, String apellidos, String telefono, String correo, String hojaDeVida, Integer idCarrera, String fotoUrl, LocalDateTime fechaCre) {
+        this.carnet = carnet;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.hojaDeVida = hojaDeVida;
+        this.idCarrera = idCarrera;
+        this.fotoUrl = fotoUrl;
+        this.fechaCre = fechaCre;
+    }
+}
---
+++ industrialPasantias/src/main/java/com/industrial/pasantias/Repository/EstudianteRepository.java
@@ -0,0 +1,11 @@
+package com.industrial.pasantias.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.industrial.pasantias.Model.Estudiante;
+
+public interface EstudianteRepository extends JpaRepository<Estudiante, String> {
+    
+    // 
+    
+}
