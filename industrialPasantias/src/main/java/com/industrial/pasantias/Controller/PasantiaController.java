a8372bac8a49d73ec71aab4b8610729ca2f919d0
DMancia03 <diego.mancia.03@gmail.com>
2024-12-03 20:12:51 -0600

    Agregando selectlistitem para utilizar en los select

--- industrialPasantias/src/main/java/com/industrial/pasantias/Controller/PasantiaController.java
+++ industrialPasantias/src/main/java/com/industrial/pasantias/Controller/PasantiaController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.industrial.pasantias.Model.Carrera;
+import com.industrial.pasantias.Model.Empresa;
 import com.industrial.pasantias.Model.EmpresaPrograma;
 import com.industrial.pasantias.Model.Pasantia;
 import com.industrial.pasantias.Servicio.CarreraService;
+import com.industrial.pasantias.Servicio.EmpresaService;
 import com.industrial.pasantias.Servicio.PasantiaService;
 import com.industrial.pasantias.Servicio.ProgramaService;
 import com.industrial.pasantias.ViewModel.SelectListItem;
@@ -29,6 +31,9 @@ public class PasantiaController {
     @Autowired
     private ProgramaService programaService;
 
+    @Autowired
+    private EmpresaService empresaService;
+
     // Index -----------------------------------------------------------
     @GetMapping
     public String listarPasantias(Model model) {
@@ -41,14 +46,28 @@ public class PasantiaController {
     @GetMapping("/nuevo")
     public String nuevaPasantia(Model model) {
         List<Carrera> carreras = carreraService.obtenerTodos();
-        List<EmpresaPrograma> programas = programaService.ObternerTodo();
-        List<SelectListItem> estados = new ArrayList<SelectListItem>();
+        // Programas de empresa
+        List<EmpresaPrograma> programasEmpresas = programaService.ObternerTodo();
+        List<SelectListItem> programas = new ArrayList<>();
+        for (EmpresaPrograma p : programasEmpresas) {
+            //Empresa empresa = empresaService.get
+            SelectListItem programa = new SelectListItem(String.valueOf(p.getIdPrograma()), "");
+            programas.add(programa);
+        }
+
+        // Estados
+        List<SelectListItem> estados = new ArrayList<>();
+        SelectListItem estado1 = new SelectListItem("E", "En Proceso");
+        estados.add(estado1);
+        estado1 = new SelectListItem("F", "Finalizado");
+        estados.add(estado1);
         Pasantia pasantia = new Pasantia();
 
         pasantia.setCorrelativo(1);
 
         model.addAttribute("carreras", carreras);
         model.addAttribute("programas", programas);
+        model.addAttribute("estados", estados);
         model.addAttribute("pasantia", pasantia);
         
         return "pasantias/guardar";
