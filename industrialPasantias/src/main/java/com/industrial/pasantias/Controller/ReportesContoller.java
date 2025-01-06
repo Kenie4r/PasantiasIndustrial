7a06a43bc9dbf5dd9bbbfa05656bd7fadd6869b2
Keneth <knthnolasco@gmail.com>
2025-01-05 18:55:53 -0600

    Subo Login

--- industrialPasantias/src/main/java/com/industrial/pasantias/Controller/ReportesContoller.java
+++ industrialPasantias/src/main/java/com/industrial/pasantias/Controller/ReportesContoller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.industrial.pasantias.Servicio.CarreraService;
@@ -18,7 +19,17 @@ public class ReportesContoller {
 
     @GetMapping("/reporteria")
     public String getMethodName(Model model) {
-        
+        model.addAttribute("reporte", ""); 
+        model.addAttribute("reporte2", ""); 
+        model.addAttribute("carreras",carreraService.obtenerTodos()); 
+        return "reportes/reporteria";
+    }
+
+    
+    @GetMapping("/reporteria/{reporte}")
+    public String reportesFiltrados(@PathVariable String reporte , Model model) {
+        model.addAttribute("reporte", reporte); 
+        model.addAttribute("reporte2", reporte); 
         model.addAttribute("carreras",carreraService.obtenerTodos()); 
         return "reportes/reporteria";
     }
