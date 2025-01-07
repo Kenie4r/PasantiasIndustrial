1ea83c833fae3d750ea3177931a2936066f1f019
DMancia03 <diego.mancia.03@gmail.com>
2025-01-07 12:07:06 -0600

    Implementando logica para cambiar estado de pasantia

--- industrialPasantias/src/main/java/com/industrial/pasantias/Controller/PasantiaController.java
+++ industrialPasantias/src/main/java/com/industrial/pasantias/Controller/PasantiaController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.core.Local;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -198,5 +200,16 @@ public class PasantiaController {
     public ResponseEntity<?> obtenerCorrelativoPorCarrera(@PathVariable Integer id){
         Integer correlativo = pasantiaService.ObtenerCorrelativoParaNuevaPasantia(id);
         return ResponseEntity.ok(correlativo);
+    }
+
+    @PostMapping("/cambiarEstadoPractica")
+    public ResponseEntity<?> cambiarEstadoPractica(@RequestBody Pasantia pasantia){
+        pasantia = pasantiaService.obtenerPorIdPasantia(pasantia.getIdPasantia());
+        
+        String estadoNuevo = pasantia.getEstado().equals("E") ? "F" : "E";
+        
+        pasantia.setEstado(estadoNuevo);
+        pasantiaService.guardar(pasantia);
+        return ResponseEntity.ok(pasantia);
     }
 }
