4d1651b231555f583ac5e02f40a8a8494dae6a1b
unknown <andres.fernandezgit@gmail.com>
2025-01-13 21:22:45 -0600

    Correcion Entity Cambio de Carrera

--- industrialPasantias/src/main/java/com/industrial/pasantias/Model/CambioDeCarrera.java
+++ industrialPasantias/src/main/java/com/industrial/pasantias/Model/CambioDeCarrera.java
@@ -3,6 +3,9 @@ package com.industrial.pasantias.Model;
 import java.sql.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +16,8 @@ import lombok.Setter;
 @Table(name = "LOG_CAMBIO_CARRERA", schema = "dbo")
 public class CambioDeCarrera {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int ID_LOG;
     private String CARNET;
 
